1e566c0941d6b7f00320135c3378537226e5d580
Raheel <dt.raheel@gmail.com>
2018-04-07 16:18:48 +0530

    Fix

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.static('./dist'));
+app.get('*',(req,res)=>{
+    res.sendFile(__dirname+'/dist/index.html')
+})
 app.listen(80,()=>{
     console.log("Server Started")
 })
