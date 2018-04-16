f2075ca2e7ef34a6e3d2638884c3bf6551701db6
Raheel <dt.raheel@gmail.com>
2018-04-16 14:57:06 +0530

    updates

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('ssl/private.key', 'utf8');
+var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
 app.use(express.static('./dist'));
 app.get('*',(req,res)=>{
-    res.sendFile(__dirname+'/dist/index.html')
+            res.sendFile(__dirname+'/dist/index.html')
 })
-app.listen(80,()=>{
-    console.log("Server Started")
-})
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+httpServer.listen(80);
+httpsServer.listen(443);
